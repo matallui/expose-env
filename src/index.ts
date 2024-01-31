ca40232eb3f5e97daee5ce1f52f710f8a3096b8d
Luis Taniça <matallui@gmail.com>
2024-01-31 11:02:14 -0700

    first implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env node
+
+import { execSync } from "child_process";
+
+function injectEnvVariables(cmd: string) {
+  const env = { ...process.env };
+  Object.keys(env).forEach((key) => {
+    const val = env[key];
+    if (val) {
+      const regex = new RegExp(`%${key}%`, "g");
+      cmd = cmd.replace(regex, val);
+    }
+  });
+  return cmd;
+}
+
+const args = process.argv.slice(2);
+
+const cmdWithEnv = injectEnvVariables(args.join(" "));
+
+execSync(cmdWithEnv, { stdio: "inherit" });
